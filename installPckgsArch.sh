8b9c571c7af96f1b0c918e75466089504cf36da3
chuvachellaV1 <tenebraehorribiles@gmail.com>
2026-02-19 23:25:52 +0200

    Update installPckgsArch.sh

--- installPckgsArch.sh
+++ installPckgsArch.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
+sudo systemctl enable --now NetworkManager
+if ping -c google.com $> /dev/null; then
+  echo "There is network  connection."
+else 
+  echo "NO INTERNET"
+  exit 1
+fi
 sudo pacman -Syu
-USER="sigma"
-mkdir -p /home/sigma/customisation
-INSTALLATION_PATH="/home/sigma/customisation"
-sudo pacman -S
+mkdir -p /home/$USER/customisation
+INSTALLATION_PATH="/home/$USER/customisation"
+sudo pacman -S --needed networkmanager nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber ttf-liberation ttf-jetbrains-mono-nerd
+sudo pacman -S --needed kitty alacritty zsh  
