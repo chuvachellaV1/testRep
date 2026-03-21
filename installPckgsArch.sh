c5c00c0db2601a2f6886dfa9c14343a322a513a4
chuvachellaV1 <tenebraehorribiles@gmail.com>
2026-03-21 18:07:06 +0200

    Update installPckgsArch.sh

--- installPckgsArch.sh
+++ installPckgsArch.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 sudo systemctl enable --now NetworkManager
 if ping -c google.com $> /dev/null; then
@@ -9,5 +10,6 @@ fi
 sudo pacman -Syu
 mkdir -p /home/$USER/customisation
 yay -S nvidia-open nvidia-utils lib32-nvidia-utils nvidia-settings
+#https://github.com/korvahannu/arch-nvidia-drivers-installation-guide/tree/a46d7f0a0c2bfbda9469b51b9b2b93323e69cf6e
 #sudo pacman -S --needed networkmanager nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber ttf-liberation ttf-jetbrains-mono-nerd
 #sudo pacman -S --needed kitty alacritty zsh  
